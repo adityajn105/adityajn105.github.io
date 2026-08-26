bb753ad1df2a15938699667ddf255a50f064c456
Aditya Jain <adityajn105@gmail.com>
2026-08-26 22:41:58 +0000

    add worflow

--- node_modules/google-logging-utils/build/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './logging-utils';
-export type { LogFields, LogSeverity, AdhocDebugLogFunction, DebugLogBackend, } from './types';
--- node_modules/proto3-json-serializer/build/src/bytes.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function bytesToProto3JSON(obj: Buffer | Uint8Array): string;
-export declare function bytesFromProto3JSON(json: string): Buffer<ArrayBuffer>;
